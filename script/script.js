bf258aad0a8a0bec800d356ac8e6c9da73cc6797
Lucas Renan <lucasrenandns96@gmail.com>
2022-07-11 13:36:52 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,21 +2,21 @@
 const slider = document.querySelectorAll('.slider')
 let index = 0
 
-const anterior = document.querySelector("#before")
-anterior.addEventListener('click', before)
-
 const proximo = document.querySelector("#next")
 proximo.addEventListener('click', next)
 
-function before() {
-    slider[index].classList.remove('active')
-    index = (index - 1 + slider.length) % slider.length
-    slider[index].classList.add('active')
-}
+const anterior = document.querySelector("#before")
+anterior.addEventListener('click', before)
 
 function next() {
     slider[index].classList.remove('active')
     index = (index + 1) % slider.length
+    slider[index].classList.add('active')
+}
+
+function before() {
+    slider[index].classList.remove('active')
+    index = (index - 1 + slider.length) % slider.length
     slider[index].classList.add('active')
 }
 
