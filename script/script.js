7fc8e1aa040fc9768235d54328983df5fb21a0e2
Lucas Renan <lucasrenandns96@gmail.com>
2022-07-11 11:34:30 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -10,13 +10,13 @@ proximo.addEventListener('click', next)
 
 function before() {
     slider[index].classList.remove('active')
-    index = (index + 1) % slider.length
+    index = (index - 1 + slider.length) % slider.length
     slider[index].classList.add('active')
 }
 
 function next() {
     slider[index].classList.remove('active')
-    index = (index - 1 + slider.length) % slider.length
+    index = (index + 1) % slider.length
     slider[index].classList.add('active')
 }
 
